26682d544d5bfccd94eb65ac7eebd1edd2ca40d0
Laszlo Teveli <tevelee@gmail.com>
2022-08-21 19:00:16 +0200

    Initial Commit

---
+++ Sudoku/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Sudoku
+//
+//  Created by laszlo.teveli on 2022. 08. 21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by laszlo.teveli on 2022. 08. 21..
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
